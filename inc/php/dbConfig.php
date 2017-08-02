77ffad203390eac5214e1012e32c721879ab796f
DenisTDR <denis.tdr@gmail.com>
2017-08-02 14:36:36 +0300

    updated db config

--- inc/php/dbConfig.php
+++ inc/php/dbConfig.php
@@ -1,38 +1,38 @@
 <?php
-    require_once("baseFuncs.php");
-    if(urlHostEquals('localhost') || urlHostEquals('127.0.0.1') || urlHostEquals('192.168.0.101')){
-        $dbHost='localhost';
-        $dbName='ebt';
-        $dbUser='ebt';
-        $dbPass='';
-    }
-    else if(urlHostEquals('ebt.kwix.eu')){
-        $dbHost='mysql.kwix.eu';
-        $dbName='u427787787_ebt';
-        $dbUser='u427787787_ebt';
-        $dbPass='romania';
-    }
-    else if(urlHostEquals('192.168.0.100')){
-        $dbHost='192.168.0.101';
-        $dbName='ebt';
-        $dbUser='ebt';
-        $dbPass='';
-    }
-    else if(urlHostEquals('ekibizz.tdr')){
-        $dbHost='localhost';
-        $dbName='ebt';
-        $dbUser='root';
-        $dbPass='';
-    }
-    else if(urlHostEquals('ekibizz.tdrs.me')){
-        $dbHost='localhost';
-        $dbName='ekibizz_2016';
-        $dbUser='ekibizz';
-        $dbPass='parola';
-    }
-    else{
-        echo "no DB config for ".parse_url(currentPageURL(), PHP_URL_HOST)."!";
-        die("");
-    }
+require_once("baseFuncs.php");
+if (urlHostEquals('localhost') || urlHostEquals('127.0.0.1') || urlHostEquals('192.168.0.101')) {
+    $dbHost = 'localhost';
+    $dbName = 'ebt';
+    $dbUser = 'ebt';
+    $dbPass = '';
+} else if (urlHostEquals('ebt.kwix.eu')) {
+    $dbHost = 'mysql.kwix.eu';
+    $dbName = 'u427787787_ebt';
+    $dbUser = 'u427787787_ebt';
+    $dbPass = 'romania';
+} else if (urlHostEquals('192.168.0.100')) {
+    $dbHost = '192.168.0.101';
+    $dbName = 'ebt';
+    $dbUser = 'ebt';
+    $dbPass = '';
+} else if (urlHostEquals('ekibizz.tdr')) {
+    $dbHost = 'localhost';
+    $dbName = 'ekibizz_2017';
+    $dbUser = 'root';
+    $dbPass = '';
+} else if (urlHostEquals('ekibizz.tdrs.me')) {
+    $dbHost = 'localhost';
+    $dbName = 'ekibizz_2016';
+    $dbUser = 'ekibizz';
+    $dbPass = 'parola';
+} else if (urlHostEquals('ekibizz.com')) {
+    $dbHost = 'localhost';
+    $dbName = 'ekibizz_2017';
+    $dbUser = 'ekibizz';
+    $dbPass = 'ekibizz2017';
+} else {
+    echo "no DB config for " . parse_url(currentPageURL(), PHP_URL_HOST) . "!";
+    die("");
+}
 
 ?>
