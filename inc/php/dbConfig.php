fcc75a1bcf769cb18a13e58984fc09e7b71ae4aa
Denis TDR <denis.tdr@gmail.com>
2018-12-02 04:56:16 +0200

    updated db config

--- inc/php/dbConfig.php
+++ inc/php/dbConfig.php
@@ -5,31 +5,16 @@ if (urlHostEquals('localhost') || urlHostEquals('127.0.0.1') || urlHostEquals('1
     $dbName = 'ebt';
     $dbUser = 'ebt';
     $dbPass = '';
-} else if (urlHostEquals('ebt.kwix.eu')) {
-    $dbHost = 'mysql.kwix.eu';
-    $dbName = 'u427787787_ebt';
-    $dbUser = 'u427787787_ebt';
-    $dbPass = 'romania';
-} else if (urlHostEquals('192.168.0.100')) {
-    $dbHost = '192.168.0.101';
-    $dbName = 'ebt';
-    $dbUser = 'ebt';
-    $dbPass = '';
-} else if (urlHostEquals('ekibizz.tdr')) {
-    $dbHost = 'localhost';
-    $dbName = 'ekibizz_2017';
-    $dbUser = 'root';
-    $dbPass = '';
 } else if (urlHostEquals('ekibizz.tdrs.me')) {
     $dbHost = 'localhost';
-    $dbName = 'ekibizz_2016';
+    $dbName = 'ekibizz_2017';
     $dbUser = 'ekibizz';
     $dbPass = 'parola';
 } else if (urlHostEquals('ekibizz.com')) {
     $dbHost = 'localhost';
-    $dbName = 'ekibizz_2017';
+    $dbName = 'ekibizz_2018';
     $dbUser = 'ekibizz';
-    $dbPass = 'ekibizz2017';
+    $dbPass = 'ekibizz';
 } else {
     echo "no DB config for " . parse_url(currentPageURL(), PHP_URL_HOST) . "!";
     die("");
